19e815b9001f9359f305b2dba2cf98a5539ab724
Kokken <raspberry@kokken.no>
2018-02-06 12:43:38 +0000

    mic sample

--- raspiImport.py
+++ raspiImport.py
@@ -2,13 +2,17 @@ import numpy as np
 
 def raspiImport(path, channels):
     with open(path, 'rb') as file:
-        samplePeriod = np.fromFile(path, dtype=double, count=1) * 1.0e-06
-        adcData = np.fromFile(path, dtype=uint16)
-    
+        samplePeriod = np.fromfile(path, dtype=float, count=1) * 1.0e-06
+        adcData = np.fromfile(path, dtype=int)
+
     sampleNumber = len(adcData) / channels
-    rawData = np.zeros(sampleNumber)
+    rawData = np.zeros(int(sampleNumber))
     for i in range(0, sampleNumber - 1):
         for j in range(0, channels - 1):
             rawData[i, j] = adcData[i * channels + j]
-            
-print("ja")
+
+    return rawData
+
+sampleData = raspiImport("./samples.bin", 5)
+
+print(sampleData[0, 0])
