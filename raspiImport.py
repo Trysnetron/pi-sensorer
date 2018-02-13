43e061ca4d0f1a07af9323e886e4ed7ca5bafb27
Kokken <raspberry@kokken.no>
2018-02-13 15:10:48 +0000

    radar sample

--- raspiImport.py
+++ raspiImport.py
@@ -1,23 +1,23 @@
 import numpy as np
 
 def raspiImport(path, channels):
-    
-    samplePeriod = np.fromfile(path, dtype="double", count=1) * 1.0e-06
-    adcData = np.fromfile(path, dtype="uint16")
-    np.delete(adcData, [0, 1, 2, 3])
-    
-    sampleNumber = int(len(adcData) / channels)
-    print(len(adcData))
-    print(len(adcData) / channels)
-    print(sampleNumber)
-    
-#   rawData = np.zeros(sampleNumber)
-#    for i in range(0, sampleNumber - 1):
-#        for j in range(0, channels - 1):
-#            rawData[i, j] = adcData[i * channels + j]
+	resolution = 4096
+	sample_period = np.fromfile(path, dtype="double", count=1) * 1.0e-06
+	adc_data = np.fromfile(path, dtype="uint16")
+	adc_data = np.delete(adc_data, [0, 1, 2, 3])
 
-#    return rawData
-    return 0
-sampleData = raspiImport("./samples.bin", 5)
+	sample_number = int(len(adc_data) / channels)
+	print(str(sample_number) + " samples per channel")
+    
+	samples = []
+	for i in range(0, channels):
+		current_channel_samples = np.zeros(sample_number)
+		for j in range(0, sample_number):
+			current_channel_samples[j] = adc_data[i * channels + j] - resolution / 2
+		samples.append(current_channel_samples)
 
-#print(sampleData[0, 0])
+	return samples
+
+sample_data = raspiImport("./samples.bin", 5)
+
+print(sample_data)
